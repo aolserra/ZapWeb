52918d8e5eceb8511c664df93cc8631950da3042
Anderson Serra <aolserra@gmail.com>
2020-11-02 15:33:32 -0300

    Versao Web do ZapWeb

---
+++ ZapWeb/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using ZapWeb.Models;
+
+namespace ZapWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+        public ActionResult Cadastro()
+        {
+            return View();
+        }
+        public ActionResult Conversacao()
+        {
+            return View();
+        }
+        //public ActionResult Login() => View();
+        //public ActionResult Cadastro() => View();
+        //public ActionResult Conversacao() => View();
+    }
+}
